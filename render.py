Fix handling of rotation

render.py
@@ -388,13 +388,12 @@ class Model:
         for elementdef in modeldef.elements:
             self.elements.append(Element(modeldef, elementdef))
 
-    def render(self, model, view, projection, mode="color", modelref={}, rotation=0):
+    def render(self, model, view, projection, mode="color", modelref={}):
         m = np.eye(4, dtype=np.float32)
         if "x" in modelref:
             m = np.dot(m, transforms.rotate(modelref["x"], (1, 0, 0)))
         if "y" in modelref:
             m = np.dot(m, transforms.rotate(modelref["y"], (0, 1, 0)))
-        m = np.dot(m, transforms.rotate(-rotation * 90, (0, 1, 0)))
 
         uvlock = modelref.get("uvlock", False)
         for element in self.elements:
@@ -414,14 +413,14 @@ class Block:
             modelrefs.append((self.models[model.name], transformation))
         return modelrefs
 
-    def render(self, variant, model, view, projection, mode="color", rotation=0):
+    def render(self, variant, model, view, projection, mode="color"):
         variant_str = mcmodel.encode_variant(variant)
         if variant_str not in self.variants:
             self.variants[variant_str] = self._load_variant(variant)
 
         modelrefs = self.variants[variant_str]
         for glmodel, transformation  in modelrefs:
-            glmodel.render(model, view, projection, mode=mode, modelref=transformation, rotation=rotation)
+            glmodel.render(model, view, projection, mode=mode, modelref=transformation)
 
 def create_transform_ortho(aspect=1.0, view="isometric", fake_ortho=True):
     model = np.eye(4, dtype=np.float32)
@@ -453,3 +452,6 @@ def create_transform_perspective(aspect=1.0):
     projection = transforms.perspective(45.0, aspect, 2.0, 50.0)
     return model, view, projection
 
+def apply_model_rotation(model, rotation=0, phi=0.0):
+    rotation = transforms.rotate(-rotation * 90 + phi, (0, 1, 0))
+    return np.dot(rotation, model)

visualize.py
@@ -12,7 +12,7 @@ import render
 
 assets = mcmodel.Assets("assets")
 #blockstate = random.choice(assets.blockstates)
-blockstate = assets.get_blockstate("minecraft/blockstates/comparator.json")
+blockstate = assets.get_blockstate("minecraft/blockstates/test_orientation.json")
 
 glblock = render.Block(blockstate)
 variants = blockstate.variants
@@ -108,11 +108,11 @@ class Canvas(app.Canvas):
         rotation = self.rotation_index
         if self.run_phi:
             self.phi += 0.2
-        actual_model = np.dot(transforms.rotate(self.phi, (0, 1, 0)), self.model)
+        actual_model = render.apply_model_rotation(self.model, rotation=rotation, phi=self.phi)
 
         current_variant = variants[self.variant_index]
         current_mode = modes[self.mode_index]
-        glblock.render(current_variant, actual_model, self.view, self.projection, mode=current_mode, rotation=rotation)
+        glblock.render(current_variant, actual_model, self.view, self.projection, mode=current_mode)
 
         v = lambda *a: np.array(a, dtype=np.float32)
         render.draw_line(v(0, 0, 0), v(10, 0, 0), actual_model, self.view, self.projection, color=(1, 0, 0, 1))